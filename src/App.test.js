57f3fddcdf092437a6a8583b43e392ace101c87a
Sam Delacruz <samdelacruz91@gmail.com>
2017-03-19 22:50:39 +0000

    Fix broken test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App />
+    </Provider>,
+    div
+  );
 });
